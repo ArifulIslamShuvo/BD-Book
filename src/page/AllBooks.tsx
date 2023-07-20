9092b185841bf3dff9a5b18c88b17ea8e04166ae
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-07-20 23:28:18 +0600

    [DONW] seacrh specific book

--- src/page/AllBooks.tsx
+++ src/page/AllBooks.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
@@ -7,22 +8,73 @@
 import { Link } from "react-router-dom";
 import { useGetBooksQuery } from "../redux/features/product/bookApi";
 import { IBooks } from "../types/globalTypes";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
+import { setSearchTerm } from "../redux/features/product/searchSlice";
 
 export default function AllBooks() {
   const { data, isLoading, error } = useGetBooksQuery(undefined);
 
+  const searchTerm = useAppSelector((state) => state.searchBook);
+  const dispatch = useAppDispatch();
+
+  const handleSearchChange = (e: { target: { value: any } }) => {
+    const searchTerm = e.target.value;
+    dispatch(setSearchTerm(searchTerm));
+  };
+
+  const filteredData = data?.filter(
+    (item: { title: string; author: string; genre: string }) => {
+      const itemValues = Object.values(item).map((value) =>
+        value.toString().toLowerCase()
+      );
+
+      return itemValues.some((value) =>
+        value.includes(searchTerm.toLowerCase())
+      );
+    }
+  );
+ 
+  
+  
   return (
     <div className="text-center py-8">
       <h2 className="font-bold text-3xl">All Books Catalog</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ml-10 mt-10">
-        {data?.map((book: IBooks) => (
+
+      <div className="max-w-md mx-auto flex p-3 my-5 rounded-lg ">
+      <input
+       onChange={handleSearchChange}
+        type="text"
+        className="w-full border border-gray-300 rounded-l-md py-2 px-3 focus:outline-none focus:border-blue-500"
+        placeholder="Search..."
+      />
+      <button className="bg-blue-600 text-white px-4 py-2 rounded-r-md">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M15 15l5-5-5-5"
+          />
+        </svg>
+      </button>
+    </div>
+
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 ml-10 mt-10">
+        {filteredData?.map((book: IBooks) => (
           <div
             key={book._id}
             className="card card-compact md:w-96 bg-red-100 shadow-xl"
           >
             <Link to={`/book-details/${book?._id}`}>
               <figure>
-                <img src={book?.img} className="h-40" alt="Shoes" />
+                <img src={book?.img} className="h-40 pt-5" alt="Shoes" />
               </figure>
             </Link>
 
