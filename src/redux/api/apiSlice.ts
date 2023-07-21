5a9c7efd929f1fed7168dce1e9a6f0d06b1ae61d
ArifulIslam Shuvo <arifulislams063@gmail.com>
2023-07-21 21:35:39 +0600

    vercel link update

--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -3,7 +3,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://bd-book-backend-lrd25cxt4-arifulislamshuvo.vercel.app/",
+    baseUrl: "https://bd-book-backend.vercel.app/",
+    // baseUrl: "https://bd-book-backend-lrd25cxt4-arifulislamshuvo.vercel.app/",
   }),
   tagTypes: ["books"],
 
